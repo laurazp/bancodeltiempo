b51c6a411c49bc9cae563f44f4e0d8cb3a73d430
laurazp <lzafrap@uoc.edu>
2022-04-03 13:40:08 +0100

    Primera prueba. Creación proyecto WPF con .NET Framework

---
+++ bancodeltiempo/Usuario.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bancodeltiempo
+{
+    internal class Usuario
+    {
+        // Attributes
+
+        private String idUsuario;
+
+        private String nombre;
+
+        private String apellidos;
+
+        private String direccion;
+
+        private String localidad;
+
+        private String provincia;
+
+        private String email;
+
+        private String telefono;
+
+        private DateTime fechaAlta;
+
+        private String usuario;
+
+        private String password;
+
+        
+        // Operations
+
+        public void actualizarDatos()
+        {
+            
+
+        }
+
+        
+        public void solicitarBaja()
+        {
+            
+
+        }
+
+        
+        public void cambiarPassword()
+        {
+            
+
+        }
+    }
+}
